ab9f382363512699faafa38916ddee2b0326d2d0
stailgot <sanchesz@list.ru>
2016-11-24 23:16:50 +0300

    clean

--- example/main.cpp
+++ example/main.cpp
@@ -2,19 +2,51 @@
 #include <nvapi.h>
 #include <NvApiDriverSettings.h>
 #include <string>
+#include <vector>
+#include <iostream>
 
 #pragma comment(lib, "nvapi.lib" )
 
+namespace nv{ namespace info {
 
-#define PrintError(x)
+void print_error(NvAPI_Status status)
+{
+  NvAPI_ShortString error;
+  NvAPI_GetErrorMessage(status, error);
+  std::cout << error << "\n";
+}
 
-bool DisplayProfileContents(NvDRSSessionHandle hSession, NvDRSProfileHandle hProfile)
+void print_setting_info(NVDRS_SETTING & setting)
+{
+  ::NvAPI_DRS_GetSettingNameFromId(setting.settingId, &setting.settingName);
+  wprintf(L"Setting Name: %s\n", setting.settingName);
+  printf("Setting ID: %X\n", setting.settingId);
+  printf("Predefined? : %d\n", setting.isCurrentPredefined);
+  switch (setting.settingType) {
+  case NVDRS_DWORD_TYPE:
+    printf("Setting Value: %X\n", setting.u32CurrentValue);
+    break;
+  case NVDRS_BINARY_TYPE: {
+    unsigned int len;
+    printf("Setting Binary (length=%d) :", setting.binaryCurrentValue.valueLength);
+    for (len = 0; len < setting.binaryCurrentValue.valueLength; len++) {
+      printf(" %02x", setting.binaryCurrentValue.valueData[len]);
+    }
+    printf("\n");
+  } break;
+  case NVDRS_WSTRING_TYPE:
+    wprintf(L"Setting Value: %s\n", setting.wszCurrentValue);
+    break;
+  }
+}
+
+bool display_profile_contents( NvDRSSessionHandle hSession, NvDRSProfileHandle hProfile )
 {
   NvAPI_Status status;
 
-  NVDRS_PROFILE profileInformation = { 0 };
-  profileInformation.version       = NVDRS_PROFILE_VER;
-  status                           = NvAPI_DRS_GetProfileInfo(hSession, hProfile, &profileInformation);
+  NVDRS_PROFILE profileInformation = { NVDRS_PROFILE_VER };
+
+  status = ::NvAPI_DRS_GetProfileInfo(hSession, hProfile, &profileInformation);
   if (status != NVAPI_OK) {
     return false;
   }
@@ -24,14 +56,16 @@ bool DisplayProfileContents(NvDRSSessionHandle hSession, NvDRSProfileHandle hPro
   printf("Is Predefined: %d\n", profileInformation.isPredefined);
 
   if (profileInformation.numOfApps > 0) {
-    NVDRS_APPLICATION* appArray    = new NVDRS_APPLICATION[profileInformation.numOfApps];
-    NvU32              numAppsRead = profileInformation.numOfApps, i;
+    std::vector<NVDRS_APPLICATION> appArray(profileInformation.numOfApps);
 
-    appArray[0].version = NVDRS_APPLICATION_VER;
-    status              = NvAPI_DRS_EnumApplications(hSession, hProfile, 0, &numAppsRead, appArray);
+    NvU32 numAppsRead = profileInformation.numOfApps;
+    NvU32 i;
+
+    appArray[0] = { NVDRS_APPLICATION_VER };
+
+    status = ::NvAPI_DRS_EnumApplications(hSession, hProfile, 0, &numAppsRead, appArray.data());
     if (status != NVAPI_OK) {
-      PrintError(status);
-      delete[] appArray;
+      print_error(status);
       return false;
     }
     for (i = 0; i < numAppsRead; i++) {
@@ -39,79 +73,64 @@ bool DisplayProfileContents(NvDRSSessionHandle hSession, NvDRSProfileHandle hPro
       wprintf(L"User Friendly Name: %s\n", appArray[i].userFriendlyName);
       printf("Is Predefined: %d\n", appArray[i].isPredefined);
     }
-    delete[] appArray;
   }
   if (profileInformation.numOfSettings > 0) {
-    NVDRS_SETTING* setArray   = new NVDRS_SETTING[profileInformation.numOfSettings];
+    std::vector<NVDRS_SETTING> setArray(profileInformation.numOfSettings);
     NvU32          numSetRead = profileInformation.numOfSettings, i;
-    setArray[0].version       = NVDRS_SETTING_VER;
-    status                    = NvAPI_DRS_EnumSettings(hSession, hProfile, 0, &numSetRead, setArray);
+    setArray[0] = { NVDRS_SETTING_VER };
+
+    status = ::NvAPI_DRS_EnumSettings(hSession, hProfile, 0, &numSetRead, setArray.data());
     if (status != NVAPI_OK) {
-      PrintError(status);
+      print_error(status);
       return false;
     }
     for (i = 0; i < numSetRead; i++) {
-      if (setArray[i].settingLocation != NVDRS_CURRENT_PROFILE_LOCATION) {
+      NVDRS_SETTING & setting = setArray[i];
+
+      if (setting.settingLocation != NVDRS_CURRENT_PROFILE_LOCATION) {
         continue;
       }
-      NvAPI_DRS_GetSettingNameFromId(setArray[i].settingId, &setArray[i].settingName);
-      wprintf(L"Setting Name: %s\n", setArray[i].settingName);
-      printf("Setting ID: %X\n", setArray[i].settingId);
-      printf("Predefined? : %d\n", setArray[i].isCurrentPredefined);
-      switch (setArray[i].settingType) {
-      case NVDRS_DWORD_TYPE:
-        printf("Setting Value: %X\n", setArray[i].u32CurrentValue);
-        break;
-      case NVDRS_BINARY_TYPE: {
-        unsigned int len;
-        printf("Setting Binary (length=%d) :", setArray[i].binaryCurrentValue.valueLength);
-        for (len = 0; len < setArray[i].binaryCurrentValue.valueLength; len++) {
-          printf(" %02x", setArray[i].binaryCurrentValue.valueData[len]);
-        }
-        printf("\n");
-      } break;
-      case NVDRS_WSTRING_TYPE:
-        wprintf(L"Setting Value: %s\n", setArray[i].wszCurrentValue);
-        break;
-      }
+      print_setting_info(setting);
     }
   }
   printf("\n");
   return true;
 }
 
-void EnumerateProfilesOnSystem()
+void enumerate_profiles_on_system()
 {
   NvAPI_Status status;
   status = NvAPI_Initialize();
   if (status != NVAPI_OK)
-    PrintError(status);
+    print_error(status);
 
   NvDRSSessionHandle hSession = 0;
 
-  status = NvAPI_DRS_CreateSession(&hSession);
+  status = ::NvAPI_DRS_CreateSession(&hSession);
   if (status != NVAPI_OK)
-    PrintError(status);
+    print_error(status);
 
-  status = NvAPI_DRS_LoadSettings(hSession);
+  status = ::NvAPI_DRS_LoadSettings(hSession);
   if (status != NVAPI_OK)
-    PrintError(status);
+    print_error(status);
 
   NvDRSProfileHandle hProfile = 0;
   unsigned int       index    = 0;
-  while ((status = NvAPI_DRS_EnumProfiles(hSession, index, &hProfile)) == NVAPI_OK) {
+  while ((status = ::NvAPI_DRS_EnumProfiles(hSession, index, &hProfile)) == NVAPI_OK) {
     printf("Profile in position %d:\n", index);
-    DisplayProfileContents(hSession, hProfile);
+    display_profile_contents(hSession, hProfile);
     index++;
   }
   if (status == NVAPI_END_ENUMERATION) {
     // this is expected at the end of the enumeration
   } else if (status != NVAPI_OK)
-    PrintError(status);
+    print_error(status);
 
-  NvAPI_DRS_DestroySession(hSession);
+  ::NvAPI_DRS_DestroySession(hSession);
   hSession = 0;
 }
+
+}} //namespace nv::info
 
 int main()
 {
@@ -122,13 +141,13 @@ int main()
   if (NvAPI_Initialize() != NVAPI_OK)
     throw std::runtime_error("NVIDIA Api not initialized");
 
-  if (NvAPI_DRS_CreateSession(&_session) != NVAPI_OK)
+  if (::NvAPI_DRS_CreateSession(&_session) != NVAPI_OK)
     throw std::runtime_error("can't create session");
 
-  if (NvAPI_DRS_LoadSettings(_session) != NVAPI_OK)
+  if (::NvAPI_DRS_LoadSettings(_session) != NVAPI_OK)
     throw std::runtime_error("can't load system settings");
 
-  if (NvAPI_DRS_GetCurrentGlobalProfile(_session, &_profile) != NVAPI_OK)
+  if (::NvAPI_DRS_GetCurrentGlobalProfile(_session, &_profile) != NVAPI_OK)
     throw std::runtime_error("can't get current global profile");
 
   NvPhysicalGpuHandle nvGPUHandle[NVAPI_MAX_PHYSICAL_GPUS];
@@ -148,49 +167,49 @@ int main()
 
   NvAPI_EnumPhysicalGPUs(nvGPUHandle, &GpuCount);
 
-  NvAPI_ShortString str;
-  NvAPI_GPU_GetFullName(nvGPUHandle[0], str);
-  NvAPI_GPU_GetPCIIdentifiers(nvGPUHandle[0], &DeviceId, &SubSystemId, &RevisionId, &ExtDeviceId);
-  NvAPI_GPU_GetBusId(nvGPUHandle[0], &BusId);
-  NvAPI_GPU_GetVbiosRevision(nvGPUHandle[0], &BiosRevision);
-  NvAPI_GPU_GetVbiosOEMRevision(nvGPUHandle[0], &BiosRevisionOEM);
+  NvAPI_ShortString str{};
+  NvAPI_GPU_GetFullName          (nvGPUHandle[0], str);
+  NvAPI_GPU_GetPCIIdentifiers    (nvGPUHandle[0], &DeviceId, &SubSystemId, &RevisionId, &ExtDeviceId);
+  NvAPI_GPU_GetBusId             (nvGPUHandle[0], &BusId);
+  NvAPI_GPU_GetVbiosRevision     (nvGPUHandle[0], &BiosRevision);
+  NvAPI_GPU_GetVbiosOEMRevision  (nvGPUHandle[0], &BiosRevisionOEM);
   NvAPI_GPU_GetVbiosVersionString(nvGPUHandle[0], str);
 
-  status = NvAPI_GPU_GetBoardInfo(nvGPUHandle[0], &BoardInfo);
-  status = NvAPI_GPU_WorkstationFeatureQuery(nvGPUHandle[0], &ConfiguredFeatureMask, &ConsistentFeatureMask);
-  status = NvAPI_GPU_GetGpuCoreCount(nvGPUHandle[0], &CoreCount);
+  status = NvAPI_GPU_GetBoardInfo            (nvGPUHandle[0], &BoardInfo);
+  status = NvAPI_GPU_WorkstationFeatureQuery (nvGPUHandle[0], &ConfiguredFeatureMask, &ConsistentFeatureMask);
+  status = NvAPI_GPU_GetGpuCoreCount         (nvGPUHandle[0], &CoreCount);
 
-  NV_CHIPSET_INFO info;
-  info.version = NV_CHIPSET_INFO_VER_4;
-  status       = NvAPI_SYS_GetChipSetInfo(&info);
+  NV_CHIPSET_INFO info{ NV_CHIPSET_INFO_VER_4 };
+
+  status = NvAPI_SYS_GetChipSetInfo(&info);
   NvAPI_GetInterfaceVersionString(str);
 
-  DisplayProfileContents(_session, _profile);
+  nv::info::display_profile_contents(_session, _profile);
 
-  //if (NvAPI_DRS_GetBaseProfile(_session, &_profile) != NVAPI_OK)
+  //if (::NvAPI_DRS_GetBaseProfile(_session, &_profile) != NVAPI_OK)
   //  throw std::runtime_error("can't get current global profile");
 
-  //NvAPI_DRS_RestoreProfileDefault(_session, _profile);
+  //::NvAPI_DRS_RestoreProfileDefault(_session, _profile);
   //DisplayProfileContents(_session, _profile);
 
-  //NvAPI_DRS_SaveSettings(_session);
+  //::NvAPI_DRS_SaveSettings(_session);
 
   // unsigned int test  = (unsigned int)-0x68;
   // unsigned int index = 0;
-  // while ((status = NvAPI_DRS_EnumProfiles(_session, index, &_profile)) == NVAPI_OK) {
+  // while ((status = ::NvAPI_DRS_EnumProfiles(_session, index, &_profile)) == NVAPI_OK) {
   //  _profInfo.version = NVDRS_PROFILE_VER;
 
   //  DisplayProfileContents(_session, _profile);
 
-  //  //if (NvAPI_DRS_GetProfileInfo(_session, _profile, &_profInfo) != NVAPI_OK)
+  //  //if (::NvAPI_DRS_GetProfileInfo(_session, _profile, &_profInfo) != NVAPI_OK)
   //  //  throw std::runtime_error("can't get current global profile info");
 
   //  index++;
   //}
 
-  // EnumerateProfilesOnSystem();
+  //nv::info::enumerate_profiles_on_system();
 
-  NvAPI_DRS_DestroySession(_session);
+  ::NvAPI_DRS_DestroySession(_session);
 
   return 0;
 }
